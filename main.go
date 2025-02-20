6c71d223af6a41aa9a721e44613023a17f92a549
Daan Onstenk 560501 <560501@student.fontys.nl>
2025-02-20 14:45:31 +0100

    Commit met enter om af te sluiten

--- main.go
+++ main.go
@@ -6,7 +6,10 @@ import (
 )
 
 func berekenSnelheid(afstandKm float64, tijdUren float64) float64 {
-	omtrekKm := 2 * math.Pi * afstandKm
+	/*
+	   Berekent de snelheid in km/u.
+	*/
+	omtrekKm := 2 * math.Pi * afstandKm // Omtrek van de cirkelbaan
 	snelheidKmU := omtrekKm / tijdUren
 	return snelheidKmU
 }
@@ -20,8 +23,11 @@ func main() {
 	fmt.Print("Voer de tijd voor een volledige omwenteling in uren in: ")
 	fmt.Scanln(&tijd)
 
-	afstand *= 1000000
+	afstand *= 1000000 // Zet miljoenen km om naar km
 
 	snelheid := berekenSnelheid(afstand, tijd)
 	fmt.Printf("De planeet beweegt zich met %.2f km/u door de ruimte.\n", snelheid)
+
+	fmt.Print("Druk op Enter om af te sluiten...")
+	fmt.Scanln() // Wacht op Enter voordat het programma wordt afgesloten
 }
